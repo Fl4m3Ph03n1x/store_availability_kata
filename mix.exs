9295c3b62f6c156c3a385db0e600c8ffcb2fa753
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-03-27 14:35:52 +0100

    added docs folder

--- mix.exs
+++ mix.exs
@@ -7,7 +7,12 @@ defmodule Availabilities.MixProject do
       version: "0.1.0",
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+
+      # Docs
+      name: "Store Availability",
+      source_url: "https://github.com/Fl4m3Ph03n1x/store_availability_kata",
+      docs: docs()
     ]
   end
 
@@ -27,7 +32,15 @@ defmodule Availabilities.MixProject do
       # Dev
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.0", only: [:dev], runtime: false}
+      {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
+      {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
   end
+
+  defp docs(), do:
+    [
+      main: "Store Availability",
+      extras: ["README.md"]
+    ]
+
 end
