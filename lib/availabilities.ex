7f3f654951e22a11984f82a21f90e0d31a2c6546
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-03-27 13:53:54 +0100

    adding logic

--- lib/availabilities.ex
+++ lib/availabilities.ex
@@ -1,18 +1,75 @@
 defmodule Availabilities do
   @moduledoc """
-  Documentation for `Availabilities`.
+  Calculates availability of stores.
   """
+
+  alias Availabilities.{Availability, Params, Schedule, TimeSlotSettings}
+  alias Timex
 
   @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> Availabilities.hello()
-      :world
-
+  Returns the availability for a given store, or empty if the store is closed
   """
-  def hello do
-    :world
+  @spec build(Params.t, Date.t) :: [Availability.t] | {:error, :invalid_date}
+  def build(%Params{schedules: schedules, settings: slots}, date), do:
+    date
+    |> Timex.weekday()
+    |> num_to_weekday()
+    |> select_store_open_days(schedules)
+    |> build_slots(slots, date)
+    |> handle_response()
+
+  @spec num_to_weekday(non_neg_integer | {:error, :invalid_date}) :: {:ok, Timex.weekday_name} | {:error, :invalid_date}
+  defp num_to_weekday(1), do: {:ok, :monday}
+  defp num_to_weekday(2), do: {:ok, :tuesday}
+  defp num_to_weekday(3), do: {:ok, :wednesday}
+  defp num_to_weekday(4), do: {:ok, :thursday}
+  defp num_to_weekday(5), do: {:ok, :friday}
+  defp num_to_weekday(6), do: {:ok, :saturday}
+  defp num_to_weekday(7), do: {:ok, :sunday}
+  defp num_to_weekday(error), do: error
+
+  @spec select_store_open_days({:ok, Timex.weekday_name} | {:error, :invalid_date}, [Schedule.t]) :: {:ok, [Schedule.t]} | {:error, :invalid_date}
+  defp select_store_open_days({:ok, weekday}, schedules), do:
+    {:ok, Enum.filter(schedules, &store_open?(weekday, &1))}
+
+  defp select_store_open_days({:error, _reason} = error, _schedules), do: error
+
+  @spec store_open?(Timex.weekday_name, Schedule.t) :: boolean
+  defp store_open?(weekday, %Schedule{weekday: schedule_weekday}), do:
+    weekday == schedule_weekday
+
+
+  defp build_slots({:ok, []}, _slots, _date), do: {:ok, :store_closed}
+
+  defp build_slots({:ok, [schedule]}, %TimeSlotSettings{slot_recurrrence: recurrence, slot_duration: duration}, date) do
+
+    start_time =  schedule.start_time
+    end_time = schedule.end_time
+
+    {:ok, %Availability{
+      weekday: schedule.weekday,
+      date: date,
+      slots: slots_in_schedule(start_time, end_time, recurrence, duration, start_time)
+    }}
   end
+
+  defp build_slots({:error, _reason} = error, _slots, _date), do: error
+
+  defp slots_in_schedule(start_time, end_time, recurrence, duration, current_time, slots \\ [])
+
+  defp slots_in_schedule(start_time, end_time, recurrence, duration, current_time, slots) when current_time < end_time do
+    slot = %{
+      start_time: Time.truncate(current_time, :second),
+      end_time: current_time |> Time.add(duration, :second) |> Time.truncate(:second)
+    }
+    new_slots = [ slot | slots]
+    slots_in_schedule(start_time, end_time, recurrence, duration, Time.add(current_time, recurrence, :second), new_slots)
+  end
+
+  defp slots_in_schedule(_start_time, end_time, _recurrence, _duration, current_time, slots) when current_time >= end_time, do:
+    Enum.reverse(slots)
+
+  defp handle_response({:ok, :store_closed}), do: []
+  defp handle_response({:ok, data}), do: [data]
+  defp handle_response(error), do: error
 end
--- test/availabilities_test.exs
+++ test/availabilities_test.exs
@@ -5,6 +5,7 @@ defmodule AvailabilitiesTest do
   alias Availabilities.{TimeSlotSettings, Schedule, Availability}
 
   describe "build" do
+
     test "returns availabilities for given day if there are slots for it" do
       #arrange
       settings = %TimeSlotSettings{
@@ -16,7 +17,17 @@ defmodule AvailabilitiesTest do
         %Schedule{
           weekday: :monday,
           start_time: ~T[09:00:00],
+          end_time: ~T[14:00:00]
+        },
+        %Schedule{
+          weekday: :wednesday,
+          start_time: ~T[13:00:00],
           end_time: ~T[18:00:00]
+        },
+        %Schedule{
+          weekday: :friday,
+          start_time: ~T[09:00:00],
+          end_time: ~T[13:00:00]
         }
       ]
 
@@ -37,35 +48,55 @@ defmodule AvailabilitiesTest do
               end_time: ~T[10:00:00]
             },
             %{
+              start_time: ~T[10:00:00],
+              end_time: ~T[11:00:00]
+            },
+            %{
               start_time: ~T[11:00:00],
               end_time: ~T[12:00:00]
             },
             %{
+              start_time: ~T[12:00:00],
+              end_time: ~T[13:00:00]
+            },
+            %{
               start_time: ~T[13:00:00],
               end_time: ~T[14:00:00]
-            },
-            %{
-              start_time: ~T[15:00:00],
-              end_time: ~T[16:00:00]
-            },
-            %{
-              start_time: ~T[17:00:00],
-              end_time: ~T[18:00:00]
             }
           ]
         }
       ]
 
-      # #assert
+      # assert
       assert actual == expected
     end
 
     test "returns empty availabilities for given day if the store is closed" do
-      throw "Not implemented"
-    end
+      #arrange
+      settings = %TimeSlotSettings{
+        slot_recurrrence: 3_600,
+        slot_duration: 3_600
+      }
 
-    test "returns error if params have incorrect format" do
-      throw "Not implemented"
+      schedules = [
+        %Schedule{
+          weekday: :monday,
+          start_time: ~T[09:00:00],
+          end_time: ~T[14:00:00]
+        }
+      ]
+
+      params = %Availabilities.Params{
+        settings: settings,
+        schedules: schedules
+      }
+
+      # #act
+      actual = Availabilities.build(params, ~D[2021-05-04])
+      expected = []
+
+      # assert
+      assert actual == expected
     end
   end
 
