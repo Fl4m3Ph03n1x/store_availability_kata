34063fc180ffbead43abd70055a9f488228c4ae3
Pedro Miguel Pereira Serrano Martins <palmelapedro@gmail.com>
2021-03-28 01:06:28 +0100

    added code coverage tool

--- mix.exs
+++ mix.exs
@@ -8,6 +8,8 @@ defmodule Availabilities.MixProject do
       elixir: "~> 1.11",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: preferred_cli_env(),
 
       # Docs
       name: "Store Availability",
@@ -16,29 +18,34 @@ defmodule Availabilities.MixProject do
     ]
   end
 
-  # Run "mix help compile.app" to learn about applications.
-  def application do
-    [
+  def application,
+    do: [
       extra_applications: [:logger]
     ]
-  end
 
-  # Run "mix help deps" to learn about dependencies.
-  defp deps do
-    [
+  defp deps,
+    do: [
       {:typed_struct, "~> 0.2.1"},
 
       # Dev
       {:credo, "~> 1.5", only: [:dev, :test], runtime: false},
+      {:excoveralls, "~> 0.10", only: :test},
       {:mix_test_watch, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: [:dev], runtime: false},
       {:ex_doc, "~> 0.22", only: :dev, runtime: false}
     ]
-  end
 
-  defp docs(),
+  defp docs,
     do: [
       extras: ["README.md"],
       output: "docs"
+    ]
+
+  defp preferred_cli_env,
+    do: [
+      coveralls: :test,
+      "coveralls.detail": :test,
+      "coveralls.post": :test,
+      "coveralls.html": :test
     ]
 end
